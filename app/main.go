e357f2a9d337f577855447b8f29c60d7911b24a0
yaochao <yaochao@huobi.com>
2020-03-11 13:50:30 +0900

    init

---
+++ app/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"github.com/btcsuite/btcutil"
+	zmq "github.com/pebbe/zmq4"
+	"time"
+)
+
+func main() {
+	subZmq("tcp://-:28332")
+}
+
+func subZmq(uri string) {
+	subscriber, err := zmq.NewSocket(zmq.SUB)
+	if nil != err {
+		panic(err)
+	}
+	defer subscriber.Close()
+	err = subscriber.Connect(uri)
+	if nil != err {
+		panic(err)
+	}
+	err = subscriber.SetSubscribe("rawtx")
+	if nil != err {
+		panic(err)
+	}
+	err = subscriber.SetRcvtimeo(300 * time.Minute)
+	if nil != err {
+		panic(err)
+	}
+	for {
+		_, err := subscriber.Recv(0)
+		if nil != err {
+			panic(err)
+		}
+		txByte, err := subscriber.RecvBytes(0)
+		if nil != err {
+			panic(err)
+		}
+		_, _ = subscriber.Recv(0)
+		tx, err := btcutil.NewTxFromBytes(txByte)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("txhash ", tx.Hash().String())
+	}
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module awesomeProject2
+
+go 1.13
+
+require (
+	github.com/btcsuite/btcutil v1.0.1
+	github.com/pebbe/zmq4 v1.2.0
+)
